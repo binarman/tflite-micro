7b15a79b7298e11aef2f1a8303b6f9c4049b2621
binarman <efimov.alexander@gmail.com>
2021-07-12 14:11:04 +0300

    build specific project for speed

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-make -f tensorflow/lite/micro/tools/make/Makefile generate_projects
+#make -f tensorflow/lite/micro/tools/make/Makefile generate_projects
+make -f tensorflow/lite/micro/tools/make/Makefile generate_mobilenet_make_project 
